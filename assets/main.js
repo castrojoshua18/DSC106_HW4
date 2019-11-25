1983969eda281b572848861e2d8f060d984fbb6c
josh <jlcastro@ucsd.edu>
2019-11-24 22:36:07 -0800

    html, css, js in

--- assets/main.js
+++ assets/main.js
@@ -2,10 +2,7 @@
 
 const JSONFileName = 'https://raw.githubusercontent.com/castrojoshua18/CASTRO_A13575128_HW4/master/assets/final_chadwick.json?token=AKL7YHH3CWMSKOU7CN3254C54SECG';
 
-var genreCounts = {
-    genre: [],
-    count: []
-};
+var genreCounts = new Map();
 
 Highcharts.ajax({
     url: JSONFileName,
@@ -14,13 +11,28 @@ Highcharts.ajax({
 
         activity = JSON.parse(activity);
 
+        // for (var i = 0; i < activity['genres'].length; i++) {
+        //     to_check = activity['genres'][i];
+        //     console.log(i)
+        //     for (var j = 0; j < to_check.length; j++) {
+        //         curr_genre = to_check[j];
+        //         console.log(curr_genre)
+        //         if (genreCounts.has(curr_genre)) {
+        //             curr_val  = genreCounts.get(curr_genre)
+        //             genreCounts.set(curr_genre, curr_val+1)
+        //         }
+        //         else {
+        //             genreCounts.set(curr_genre, 0)
+        //         }
+        //     }
+        // }
+
         var timelineDiv = document.createElement('div');
         timelineDiv.className = 'timeline';
         document.getElementById('timeline').append(timelineDiv);
 
         Highcharts.chart(timelineDiv, {
             chart: {
-                zoomType: 'x',
                 type: 'timeline',
                 backgroundColor: 'transparent'
             },
@@ -39,44 +51,126 @@ Highcharts.ajax({
                 enabled: false
             },
             title: {
-                text: "How has Chadwick Boseman's role changed?"
+                text: "How have Chadwick Boseman's roles evolved?"
             },
             subtitle: {
                 text: "Fig. 1: A visualized transition of Boseman's role in featured movies (U.S. domestic)"
             },
             colors: [
-                '#8c5218',
-                '#427CDD',
-                '#406AB2',
-                '#3E5A8E',
-                '#3B4A68',
-                '#363C46'
+                '#c9af95',
+                '#b59a7f',
+                '#a3896f',
+                '#8f755b',
+                '#755d45',
+                '#614b35',
+                '#523d28',
+                '#4a341e',
+                '#452d15',
+                '#3d260f',
+                '#2e1b08'
             ],
+            credits: {
+                enabled: false
+            },
             series: [{
-                pointStart: Date.UTC(2008,10,22,0,0,0,0),
-                data: [{
+                pointStart: Date.UTC(2008, 10, 22),
+                dataLabels: {
+                    allowOverlap: false,
+                    format: '<span style="color:{point.color}">● </span><span style="font-weight: bold;" > ' +
+                '{point.label}'
+                },
+                 data: [{
                         x: Date.UTC(2008, 10, 22),
-                        name: 'Support',
-                        label: 'The Express',
-                        description: "Part: "
+                        name: 'The Express (2008)',
+                        label: 'Support',
+                        description: "Part: Floyd Little",
                     },
                     {
-                        x: Date.UTC(1951, 5, 22),
-                        name: 'Support',
-                        label: 'The Express',
-                        description: "Part: "
+                        x: Date.UTC(2013, 3, 12),
+                        name: '42',
+                        label: 'Lead',
+                        description: "Part: Jackie Robinson",
+                        dataLabels: {
+                            backgroundColor: '#ebd5c0'
+                        }
                     },
                     {
-                        x: Date.UTC(1951, 5, 22),
-                        name: 'Support',
-                        label: 'The Express',
-                        description: "Part: "
+                        x: Date.UTC(2014, 5, 22),
+                        name: 'Draft Day',
+                        label: 'Support',
+                        description: "Part: Vontae Mack"
                     },
-                    
+                    {
+                        x: Date.UTC(2014, 7, 1),
+                        name: 'Get on Up ',
+                        label: 'Lead',
+                        description: "Part: James Brown",
+                        dataLabels: {
+                            backgroundColor: '#ebd5c0'
+                        }
+                    },
+                    {
+                        x: Date.UTC(2016, 1, 26),
+                        name: 'Gods of Egypt',
+                        label: 'Support',
+                        description: "Part: Thoth"
+                    },
+                    {
+                        x: Date.UTC(2016, 4, 6),
+                        name: 'Captain America: Civil War',
+                        label: 'Support',
+                        description: "Part: T'Challa - Black Panther"
+                    },
+                    {
+                        x: Date.UTC(2017, 9, 13),
+                        name: 'Marshall',
+                        label: 'Lead',
+                        description: "Part: Thurgood Marshall",
+                        dataLabels: {
+                            backgroundColor: '#ebd5c0'
+                        }
+                    },
+                    {
+                        x: Date.UTC(2018, 1, 16),
+                        name: 'Black Panther',
+                        label: 'Lead',
+                        description: "Part: T'Challa - Black Panther",
+                        dataLabels: {
+                            backgroundColor: '#ebd5c0'
+                        }
+                    },
+                    {
+                        x: Date.UTC(2018, 3, 27),
+                        name: 'Avengers: Infinity War',
+                        label: 'Support',
+                        description: "Part: T'Challa - Black Panther"
+                    },
+                    {
+                        x: Date.UTC(2019, 3, 26),
+                        name: 'Avengers: Endgame',
+                        label: 'Support',
+                        description: "Part: T'Challa - Black Panther"
+                    },
+                    {
+                        x: Date.UTC(2019, 10, 22),
+                        name: '21 Bridges',
+                        label: 'Lead',
+                        description: "Part: Andre Davis",
+                        dataLabels: {
+                            backgroundColor: '#ebd5c0'
+                        }
+                    },
                 ]
             }]
         })
 
+        var radarDiv = document.createElement('div');
+        radarDiv.className = 'timeline';
+        document.getElementById('radar').append(radarDiv);
+
+        Highcharts.chart(radarDiv, {
+            
+        })
 
     }
 
